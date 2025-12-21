70e2025822785afc3059a50ec720d851c7cebf11
Jam-HAN <m67776444@gmail.com>
2025-12-21 13:26:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // script.js (V56.0 - Optimized UI Rendering)
 // ==========================================
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxGpFXB1QH3zPmYafvNYSvwr6pDK323ecLhmzsdVG_oeKCEeYnDSI1GIlpnCMJ-TpeU/exec"; 
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxUMhvQy7T9fESf_pYHfWpph8gxcXK-IZIOD7HBOBPj9can0c2jSKzX6l6He1zfNPKC/exec"; 
 
 let currentUser = "";
 let inPendingList = [];
